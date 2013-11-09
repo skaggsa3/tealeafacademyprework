9d95d1aca42e1d27c0a113065fc722f090f08515
Adam Skaggs <skaggsa3@gmail.com>
2013-11-09 15:47:14 -0500

    This implements my version of the sort method - this is a mid chapter exercise in chapter 10

---
+++ chapter10/new_sort2.rb
@@ -0,0 +1,35 @@
+def my_sort words
+	sorted = []
+
+	while words.length != 0
+		inserted_val = words.min
+		index_val = words.index(inserted_val)
+
+		sorted.push(inserted_val)
+
+		words.delete_at(index_val)
+	end
+	p sorted.sort
+	sorted
+end
+
+words = []
+
+puts "Please help me build an array so I can test my new sorting algorithm"
+
+while true
+	w = gets.chomp
+
+	if w.empty?
+		break
+	end
+
+	words.push(w)
+end
+
+puts "Thank you, lets sort this thing"
+sorted_words = my_sort words
+
+p sorted_words
+ 
+
